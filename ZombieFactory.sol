0963e698ecd9c7919d98b1c60a2965a56b5fa680
AiRAM-S <2579701825@qq.com>
2024-09-27 09:36:24 +0800

    correct conflict

--- ZombieFactory.sol
+++ ZombieFactory.sol
@@ -14,9 +14,17 @@ contract ZombieFactory{
     event NewZombie(uint zombieId, string name, uint dna);
 
     function _createZombie(string _name, uint _dna) private {
-        Zombie newzombie = Zombie(_name, _dna);
-        uint _zombieId = zombies.push(newzombie);
-        NewZombie(_zombieId, _name, _dna)
+        uint _zombieId = zombies.push(Zombie(_name, _dna));
+        emit NewZombie(_zombieId, _name, _dna);
+    }
+
+    function _generateRandomDna(string _str) private view returns (uint) {
+        return uint(keccak256(_str)) % dnaModulus;
+    }
+
+    function createRandomZombie(string _name) public {
+        uint randDna = _generateRandomDna(_name);
+        _createZombie(_name, randDna);
     }
 
 }
